4df97932b6ae42a38a53bb026811c4e1791ab3a4
JeraldReb <jerald.reb@gmail.com>
2026-04-09 13:01:46 -0400

    Added documentation

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,20 @@
+"""Participation Activity 12
+Jerald Reb
+Beginning to create Alien Invasion game
+Starter code from Unit 12 resources
+April 9, 2026"""
+
 import sys
 import pygame
 
 class AlienInvasion:
+    """Creates a class to house the game"""
     
     def __init__(self):
+        """Initializes the game"""
         pygame.init()
+
+        self.screen = pygame.display.set_mode
 
 if __name__ == '__main__':
     pass
