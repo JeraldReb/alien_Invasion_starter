374257a24ca9fcc22045ec7e9eb56583ed7f310d
JeraldReb <jerald.reb@gmail.com>
2026-04-10 14:58:59 -0400

    Moved ship position to the right side, changed movement to y-axis, and rotated image

--- ship.py
+++ ship.py
@@ -18,12 +18,13 @@ class Ship:
 
         self.image = pygame.image.load(self.settings.ship_file)
         self.image = pygame.transform.scale(self.image, (self.settings.ship_w, self.settings.ship_h))
+        self.image = pygame.transform.rotate(self.image, (90))
 
         self.rect = self.image.get_rect()
         self.rect.midright = self.boundaries.midright
-        self.moving_right = False
-        self.moving_left = False
-        self.x = float(self.rect.x)
+        self.moving_up = False
+        self.moving_down = False
+        self.y = float(self.rect.y)
         self.arsenal = arsenal
 
     def update(self):
@@ -34,14 +35,13 @@ class Ship:
     def _update_ship_movement(self):
         temp_speed = self.settings.ship_speed
         
-        if self.moving_right and self.rect.right < self.boundaries.right:
-            self.x += temp_speed
+        if self.moving_up and self.rect.top > self.boundaries.top:
+            self.y -= temp_speed
         
-        if self.moving_left and self.rect.left > self.boundaries.left:
-            self.x -= temp_speed
+        if self.moving_down and self.rect.bottom < self.boundaries.bottom:
+            self.y += temp_speed
 
-        self.rect.x = self.x
-
+        self.rect.y = self.y
 
     def draw(self):
         """Draws the ship on the screen"""
