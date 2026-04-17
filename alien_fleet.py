56ff738c13f7956cc2e3183e1d1bec20739dd3ab
JeraldReb <jerald.reb@gmail.com>
2026-04-17 13:05:18 -0400

    p2 - modified fleet to also create more rows

--- alien_fleet.py
+++ alien_fleet.py
@@ -21,33 +21,46 @@ class AlienFleet:
     def create_fleet(self):
         """Creates the alien fleet"""
         alien_w = self.settings.alien_w
+        alien_h = self.settings.alien_h
         screen_w = self.settings.screen_w
+        screen_h = self.settings.screen_h
 
-        fleet_w = self.calculate_fleet_size(alien_w, screen_w)
-        # half_screen = self.settings.screen_w
+        fleet_w, fleet_h = self.calculate_fleet_size(alien_w, screen_w, alien_h, screen_h)
+        half_screen = self.settings.screen_w // 2
 
         fleet_horizontal_space = fleet_w * alien_w
+        fleet_vertical_space = fleet_h * alien_h
         x_offset = int((screen_w - fleet_horizontal_space)// 2)
+        y_offset = int((half_screen - fleet_vertical_space) // 2)
 
-        for col in range(fleet_w):
-            current_x = alien_w * col + x_offset
+        for row in range(fleet_h):      
+            for col in range(fleet_w):
+                current_x = alien_w * col + x_offset
+                current_y = alien_h * row + y_offset
 
-            if col % 2 == 0:
-                continue
+                if col % 2 == 0 or row % 2 ==0:
+                    continue
 
-            self._create_alien(current_x, 10)
+                self._create_alien(current_x, current_y)
 
-    def calculate_fleet_size(self, alien_w: int, screen_w: int):
+    def calculate_fleet_size(self, alien_w, screen_w, alien_h, screen_h):
         """Calculates the size of the alien fleet"""
         fleet_w = (screen_w // alien_w)
+        fleet_h = ((screen_h / 2) // alien_h)
 
         if fleet_w % 2 == 0:
             fleet_w -= 1
 
         else:
             fleet_w -= 2
+        
+        if fleet_h % 2 == 0:
+            fleet_h -= 1
 
-        return fleet_w
+        else:
+            fleet_h -= 2
+
+        return int(fleet_w), int(fleet_h)
 
     def _create_alien(self, current_x: int, current_y: int):
         """Creates individual aliens to build the fleet"""
